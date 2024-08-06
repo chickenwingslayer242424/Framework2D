686b85c1552074bd66d220ea1021adb179ca34b7
chickenwingslayer242424 <truong.giang.tran@outlook.com>
2024-08-07 01:38:37 +0200

    Camera Pan Out

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,16 +5,50 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform player;       // Reference to the player's transform
-    public Vector3 offset;         // Offset between the camera and player
-    public float smoothSpeed = 0.3f; // How smooth the camera movement will be
+    public Vector3 offset;         // Offset between the cam and player
+    public float smoothSpeed = 0.3f; // How smooth the cam movement will be
     private Vector3 velocity = Vector3.zero;
 
-    void FixedUpdate() //bei SmoothDamp interpolate -> none sonst geht nicht
+    public Camera cam; // Reference to the Camera
+    public float zoomSpeed = 2f; // Speed at which the cam zooms
+
+    public float maxZoomOut = 10f; // Maximum zoom out limit
+    public float minZoomIn = 5f; // Minimum zoom in limit (original size)
+    private bool isZoomedOut = false; // Track zoom state
+
+
+    void Update() //bei SmoothDamp interpolate -> none sonst geht nicht
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ToggleZoom();
+        }
+    }
+    void FixedUpdate()
     {
         Vector3 targetPosition = player.position + offset;
-
-
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothSpeed);
+    }
 
+    void ToggleZoom()
+    {
+        if (isZoomedOut)
+        {
+            ZoomIn();
+        }
+        else
+        {
+            ZoomOut();
+        }
+        isZoomedOut = !isZoomedOut; // Toggle zoom state
+    }
+    public void ZoomOut()
+    {
+        cam.orthographicSize = maxZoomOut;
+    }
+    void ZoomIn()
+    {
+        // Set the orthographic size to the minimum zoom in value (original size)
+        cam.orthographicSize = minZoomIn;
     }
 }
